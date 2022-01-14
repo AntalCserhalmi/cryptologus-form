e9dfd541d515e2b67f6b6378dd863d8d0785b02d
Antal Cserhalmi <cserhalmiantalprog@gmail.com>
2022-01-14 17:51:02 +0100

    Init

--- kripto/index.js
+++ kripto/index.js
@@ -7,20 +7,81 @@ $(document).ready(function(){
                 minlength: 5,
                 required: true
             },
-            magic: {
+            q1: {
                 minlength: 1,
                 required: true
-            }
+            },
+            q2: {
+                minlength: 1,
+                required: true
+            },
+            q3: {
+                minlength: 1,
+                required: true
+            },
+            q4: {
+                minlength: 1,
+                required: true
+            },
+            q5: {
+                minlength: 1,
+                required: true
+            },
+            q6: {
+                minlength: 1,
+                required: true
+            },
+            q7: {
+                minlength: 1,
+                required: true
+            },
+            q8: {
+                minlength: 1,
+                required: true
+            },
+
         },
         messages: {
             discord: {
                 required: "Ezt a mezőt kötelező kitölteni!",
                 minlength: "Legalább 5 karakterből kell állnia a mezőnek!"
             },
-            magic: {
+            q1: {
                 minlength: "Ez a mező nem lehet üres!",
                 required: "Ezt a mezőt kötelező kitölteni!"
-            }
+            },
+            q2: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+            q3: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+            q4: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+            q5: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },            q1: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+            q6: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+            q7: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+            q8: {
+                minlength: "Ez a mező nem lehet üres!",
+                required: "Ezt a mezőt kötelező kitölteni!"
+            },
+
         },
 
         highlight: function() {
